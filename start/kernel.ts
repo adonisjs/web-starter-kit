5f086558e5dcd4a8fc5890186876e1f1648f8f3c
Julien <julien@ripouteau.com>
2023-06-08 11:41:33 +0200

    feat: add @adonisjs/static

--- start/kernel.ts
+++ start/kernel.ts
@@ -24,6 +24,7 @@ server.errorHandler(() => import('#exceptions/handler'))
  */
 server.use([
   () => import('#middleware/container_bindings_middleware'),
+  () => import('@adonisjs/static/static_middleware'),
 ])
 
 /**
