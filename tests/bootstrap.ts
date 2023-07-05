79d8327e3b8484d420f51344ec0adbd3d605022f
Harminder Virk <virk.officials@gmail.com>
2023-07-05 11:53:14 +0530

    refactor: remove api client and functional test

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -1,5 +1,4 @@
 import { assert } from '@japa/assert'
-import { apiClient } from '@japa/api-client'
 import app from '@adonisjs/core/services/app'
 import type { Config } from '@japa/runner/types'
 import { pluginAdonisJS } from '@japa/plugin-adonisjs'
@@ -13,7 +12,7 @@ import testUtils from '@adonisjs/core/services/test_utils'
  * Configure Japa plugins in the plugins array.
  * Learn more - https://japa.dev/docs/runner-config#plugins-optional
  */
-export const plugins: Config['plugins'] = [assert(), apiClient(), pluginAdonisJS(app)]
+export const plugins: Config['plugins'] = [assert(), pluginAdonisJS(app)]
 
 /**
  * Configure lifecycle function to run before and after all the
--- tests/functional/welcome.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@japa/runner'
-
-test.group('Home page', () => {
-  test('visit home page', async ({ client }) => {
-    const response = await client.get('/')
-    response.assertStatus(200)
-    response.assertTextIncludes('It wors!')
-  })
-})
