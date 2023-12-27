ad9759ff82ce8f7d206a249572d2d4ee27c22c0d
Harminder Virk <virk.officials@gmail.com>
2023-12-27 12:03:29 +0530

    chore: remove lucid dependencies as they will installed post configure

--- adonisrc.ts
+++ adonisrc.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   | will be scanned automatically from the "./commands" directory.
   |
   */
-  commands: [() => import('@adonisjs/core/commands'), () => import('@adonisjs/lucid/commands')],
+  commands: [() => import('@adonisjs/core/commands')],
 
   /*
   |--------------------------------------------------------------------------
