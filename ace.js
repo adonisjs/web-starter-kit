c49614b4ff975c774ef0d48b9a4a2d0942549621
Julien Ripouteau <julien@ripouteau.com>
2024-01-27 19:06:53 +0100

    chore: add note about ace rewrite

--- ace.js
+++ ace.js
@@ -3,6 +3,11 @@
 | JavaScript entrypoint for running ace commands
 |--------------------------------------------------------------------------
 |
+| DO NOT MODIFY THIS FILE AS IT WILL BE OVERRIDDEN DURING THE BUILD
+| PROCESS.
+|
+| See docs.adonisjs.com/guides/typescript-build-process#creating-production-build
+|
 | Since, we cannot run TypeScript source code using "node" binary, we need
 | a JavaScript entrypoint to run ace commands.
 |
